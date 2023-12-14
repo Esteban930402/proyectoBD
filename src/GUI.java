22df4135dfaf6356cca1118f6d476b6df236cab1
Esteban93040 <espinosa.esteban@correounivalle.edu.co>
2023-12-14 12:55:20 -0500

    Falta consolidado mensual

--- src/GUI.java
+++ src/GUI.java
@@ -9,12 +9,13 @@ public class GUI extends JFrame{
     //Opcion para registrar paciente en sistemas:
     public JTextArea nombreExamen,entidad,cedula,fechaNacimiento,pos,telefono,celular,correo,nombreContacto,celularContacto;
     //
-
+    public JLabel lblnroConsecutivo,lblFechaIngreso,lblFechaSolicitud,lblNombreMedico,lblNumeroOrden,lblConsultaCedula;
+    public JTextField txtnroConsecutivo,txtFechaIngreso,txtFechaSolicitud,txtNombreMedico,txtNumeroOrden,txtConsultaCedula;
     public JLabel lblCedula,lblEntidad,lblPos,lblNombreExamen,lblTelefono,lblFechaNacimiento,lblCelular,lblCorreo,lblOtraPersona,lblTelOtraPersona;
 
     public JTextField txtCedula,txtEntidad,txtPos,txtTelefono,txtCelular,txtCorreo,txtFechaNacimiento,txtNombreExamen,txtOtraPersona,txtTelOtraPersona;
     public  JPanel principalPanel,buttonPanel, panelSegundario;
-    public JButton ordenes,pacientes,generarMensual,crearOrden,consultarOrden;
+    public JButton ordenes,pacientes,generarMensual,crearOrden,consultarOrden,crearPaciente,consultarPaciente;
     //Ordenes: Crear orden(y despues ingresar datos de examenes), Consultar orden(por numero de factura)
     //Generar mensual: Todos los examenes realizados en el mes
     public  JButton obtenerDatosOrden,ObtenerDatosConsulta,volverMenuPrincipal,obtenerDatosRegistroPaciente;
@@ -65,7 +66,23 @@ public class GUI extends JFrame{
     }
 
     public void panelPacientes(){
+        crearPaciente = new JButton("Crear paciente");
+        consultarPaciente = new JButton("Consultar Paciente");
 
+        crearPaciente.addActionListener(listener);
+        consultarPaciente.addActionListener(listener);
+
+        vaciarPanelsegundario();
+
+        panelSegundario.add(consultarPaciente);
+        panelSegundario.add(crearPaciente);
+        ordenes.setEnabled(true);
+        pacientes.setEnabled(false);
+        generarMensual.setEnabled(true);
+        panelSegundario.revalidate();
+        panelSegundario.repaint();
+        principalPanel.revalidate();
+        principalPanel.repaint();
     }
 
     public void panelOrdenes(){
@@ -77,6 +94,8 @@ public class GUI extends JFrame{
         crearOrden.addMouseListener(listener);
         consultarOrden.addMouseListener(listener);
 
+        vaciarPanelsegundario();
+
         panelSegundario.add(crearOrden);
         panelSegundario.add(consultarOrden);
         ordenes.setEnabled(false);
@@ -85,10 +104,7 @@ public class GUI extends JFrame{
         principalPanel.revalidate();
         principalPanel.repaint();
 
-
-
     }
-
     public void vaciarPanelsegundario(){
 
         panelSegundario.removeAll();
@@ -97,12 +113,60 @@ public class GUI extends JFrame{
 
     }
 
-    public void recopilarDatosOrdenes(){
+    public void recopilarDatosPaciente(){
 
     }
-    public void crearOrden(){
+
+    public void panelCrearOrden(){
         vaciarPanelsegundario();
-        JPanel panelFormularioPaciente = new JPanel(new GridLayout(5, 2)); // 0 filas, 2 columnas
+        JPanel panelFormularioOrdenes = new JPanel(new GridLayout(5,2));
+
+        lblnroConsecutivo = new JLabel("Consecutivo: ");
+        txtnroConsecutivo = new JTextField(10);
+
+        lblFechaSolicitud = new JLabel("Fecha de Solicitud: ");
+        txtFechaSolicitud = new JTextField(10);
+
+        lblFechaIngreso = new JLabel("Fecha Ingresoo: ");
+        txtFechaIngreso = new JTextField(10);
+
+        lblNombreMedico = new JLabel("Nombre del medico: ");
+        txtNombreMedico = new JTextField(10);
+
+        lblNumeroOrden = new JLabel("Nombre del medico: ");
+        txtNumeroOrden = new JTextField(10);
+
+        panelFormularioOrdenes.add(lblnroConsecutivo);
+        panelFormularioOrdenes.add(txtnroConsecutivo);
+
+        panelFormularioOrdenes.add(lblFechaSolicitud);
+        panelFormularioOrdenes.add(txtFechaSolicitud);
+
+        panelFormularioOrdenes.add(lblFechaIngreso);
+        panelFormularioOrdenes.add(txtFechaIngreso);
+
+        panelFormularioOrdenes.add(lblNombreMedico);
+        panelFormularioOrdenes.add(txtNombreMedico);
+
+        panelFormularioOrdenes.add(lblNumeroOrden);
+        panelFormularioOrdenes.add(txtNumeroOrden);
+
+        JButton siguiente = new JButton("Siguiente");
+        JPanel botonSiguiente = new JPanel();
+        botonSiguiente.add(siguiente);
+
+        principalPanel.add(botonSiguiente,BorderLayout.SOUTH);
+        panelSegundario.add(panelFormularioOrdenes);
+
+        panelSegundario.revalidate();
+        panelSegundario.repaint();
+        principalPanel.revalidate();
+        principalPanel.repaint();
+
+    }
+    public void crearPaciente(){
+        vaciarPanelsegundario();
+        JPanel panelFormularioPaciente = new JPanel(new GridLayout(5, 2));
 
         // Crear etiquetas y campos de texto para los datos
         lblNombreExamen = new JLabel("  Nombre de Examen:");
@@ -181,7 +245,26 @@ public class GUI extends JFrame{
     }
 
     public void consultarOrden(){
+        vaciarPanelsegundario();
+        JPanel panelConsultarOrden = new JPanel(new GridLayout(5,2));
 
+        lblConsultaCedula = new JLabel("Ingrese numero de Cedula: ");
+        txtConsultaCedula = new JTextField(10);
+
+        panelConsultarOrden.add(lblConsultaCedula);
+        panelConsultarOrden.add(txtConsultaCedula);
+
+        JButton Consultar = new JButton("Consultar");
+        JPanel panelBotonConsultar = new JPanel();
+        panelBotonConsultar.add(Consultar);
+
+        principalPanel.add(panelBotonConsultar,BorderLayout.SOUTH);
+        panelSegundario.add(panelConsultarOrden);
+
+        panelSegundario.revalidate();
+        panelSegundario.repaint();
+        principalPanel.revalidate();
+        principalPanel.repaint();
     }
 
     public static void main(String[] args) {
@@ -202,7 +285,18 @@ public class GUI extends JFrame{
 
         if (e.getSource()==GUI.this.crearOrden){
             System.out.println("Panel");
-            crearOrden();
+            panelCrearOrden();
+        }
+        if (e.getSource()==GUI.this.consultarOrden){
+            consultarOrden();
+        }
+        if (e.getSource()==GUI.this.pacientes){
+            System.out.println("Crear");
+            panelPacientes();
+        }
+        if (e.getSource()==GUI.this.crearPaciente){
+            System.out.println("Panel paciente");
+            crearPaciente();
         }
 
 
